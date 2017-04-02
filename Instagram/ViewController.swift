56f628ee8dc61f67c3c33d9d6dbbad8cfbd3e741
PRIYESH <priyesh@PRIYESHs-MacBook-Air.local>
2017-04-02 20:09:47 +0530

    Signin ui added

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var email: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
+
+    @IBAction func fbBtnTapped(_ sender: Any) {
+        
+        
+        
+    }
+    @IBAction func signinBtnTapped(_ sender: Any) {
+        
+        
+        
+    }
 }
 
